32745d31247317f43f92e4d5e4d87b1dd4bcd452
John Rader <jcrader238@gmail.com>
2023-01-22 23:07:15 -0600

    ?

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,6 @@
-import { Html, Head, NextScript } from 'next/document';
+import {
+  Html, Head, Main, NextScript,
+} from 'next/document';
 
 export default function Document() {
   return (
@@ -9,6 +11,7 @@ export default function Document() {
         <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet" />
       </Head>
       <body>
+        <Main />
         <NextScript />
       </body>
     </Html>
